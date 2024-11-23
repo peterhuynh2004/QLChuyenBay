67feb6f2d53178a627e4997b9654f79bc8611650
peterhuynh2004 <huynhxuanchinh2004@gmail.com>
2024-11-24 02:31:27 +0700

    Initial commit

---
+++ appQLChuyenBay/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+from flask_login import LoginManager
+import cloudinary
+
+app = Flask(__name__)
+
+app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/saledb?charset=utf8mb4"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["PAGE_SIZE"] = 8
+
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+
+cloudinary.config(
+    cloud_name="dxxwcby8l",
+    api_key="448651448423589",
+    api_secret="ftGud0r1TTqp0CGp5tjwNmkAm-A",
+    secure=True
+)
