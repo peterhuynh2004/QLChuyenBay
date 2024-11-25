00812fc3d28df1de07b84f7487f4dfcf5eb702fe
peterhuynh2004 <huynhxuanchinh2004@gmail.com>
2024-11-25 10:34:15 +0700

    Initial commit

--- appQLChuyenBay/__init__.py
+++ appQLChuyenBay/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
-from flask_login import LoginManager
+# from flask_login import LoginManager
 import cloudinary
 
 app = Flask(__name__)
 
 app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/saledb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/QLChuyenBay?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 8
 
 db = SQLAlchemy(app)
 
-login = LoginManager(app)
+# login = LoginManager(app)
 
 cloudinary.config(
     cloud_name="dxxwcby8l",
--- appQLChuyenBay/index.py
+++ appQLChuyenBay/index.py
@@ -2,69 +2,13 @@ import math
 
 from flask import render_template, request, redirect
 import dao
-from app import app, login
-from flask_login import login_user, logout_user
+from appQLChuyenBay import app
+# from flask_login import login_user, logout_user
 
 
 @app.route("/")
 def index():
-    cates = dao.load_categories()
-
-    kw = request.args.get('kw')
-    cate_id = request.args.get('category_id')
-    page = request.args.get('page', 1)
-
-    prods = dao.load_products(kw=kw, category_id=cate_id, page=int(page))
-
-    total = dao.count_products()
-    return render_template('index.html', categories=cates, products=prods,
-                           pages=math.ceil(total/app.config["PAGE_SIZE"]))
-
-
-@app.route("/login", methods=['get', 'post'])
-def login_process():
-    if request.method.__eq__('POST'):
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        u = dao.auth_user(username=username, password=password)
-        if u:
-            login_user(u)
-            return redirect('/')
-
-    return render_template('login.html')
-
-
-@app.route("/logout")
-def logout_process():
-    logout_user()
-    return redirect('/login')
-
-
-@app.route('/register', methods=['get', 'post'])
-def register_process():
-    err_msg = ''
-    if request.method.__eq__('POST'):
-        password = request.form.get('password')
-        confirm = request.form.get('confirm')
-
-        if password.__eq__(confirm):
-            data = request.form.copy()
-            del data['confirm']
-
-            avatar = request.files.get('avatar')
-            dao.add_user(avatar=avatar, **data)
-
-            return redirect('/login')
-        else:
-            err_msg = 'Mật khẩu không khớp!'
-
-    return render_template('register.html', err_msg=err_msg)
-
-
-@login.user_loader
-def load_user(user_id):
-    return dao.get_user_by_id(user_id)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
