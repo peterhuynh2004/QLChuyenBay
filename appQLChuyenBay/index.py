2fe4a5a7273277ca1a45794a70f6d20a9a56a44b
peterhuynh2004 <huynhxuanchinh2004@gmail.com>
2024-11-26 13:20:40 +0700

    fixloi

--- appQLChuyenBay/index.py
+++ appQLChuyenBay/index.py
@@ -3,12 +3,28 @@ import math
 from flask import render_template, request, redirect
 import dao
 from appQLChuyenBay import app
+
+
 # from flask_login import login_user, logout_user
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/login")
+def dangnhap():
+    return render_template('login.html')
+
+@app.route("/huong_dan_dat_cho")
+def huongdandatcho():
+    return render_template('huong_dan_dat_cho.html')
+
+
+@app.route("/kiem_tra_ma")
+def kiemtrama():
+    return render_template('kiem_tra_ma.html')
 
 
 if __name__ == '__main__':
