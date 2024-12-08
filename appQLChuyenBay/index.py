0668b4706c17d02b7659eb0c8cd6a1b921e7745b
VoVanMai <maivo0902@gmail.com>
2024-12-08 16:58:23 +0700

    usecase1_xongB4

--- appQLChuyenBay/index.py
+++ appQLChuyenBay/index.py
@@ -1,12 +1,12 @@
 import math
 
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for, Flask, session
 import dao
 from appQLChuyenBay import app, login
 
 
 # from flask_login import login_user, logout_user
-
+app.secret_key = '123456'
 
 @app.route("/")
 def index():
@@ -37,7 +37,7 @@ def kiemtrama():
 @app.route("/timkiemchuyenbay")
 def timkiemchuyenbay():
     airport = dao.load_airport()
-    id_SanBayDen= request.args.get('id_SanBayDen')
+    id_SanBayDen = request.args.get('id_SanBayDen')
     id_SanBayDi = request.args.get('id_SanBayDi')
     ngayDi = request.args.get('ngayDi')
     flight = dao.load_flight(id_SanBayDen=id_SanBayDen, id_SanBayDi=id_SanBayDi, ngayDi=ngayDi)
@@ -47,9 +47,28 @@ def timkiemchuyenbay():
                            id_SanBayDi=id_SanBayDi, id_SanBayDen=id_SanBayDen)
 
 
-@app.route("/datveonline")
+
+@app.route("/datveonline", methods=['GET', 'POST'])
 def datveonline():
+    if request.method.__eq__('POST'):
+        session['fullName'] = request.form['fullName']
+        session['phone'] = request.form['phone']
+        session['email'] = request.form['email']
+        session['cccd'] = request.form['cccd']
+        return redirect(url_for('thongtindatve'))
     return render_template('datveonline.html')
+# return render_template('datveonline.html')
+
+
+
+@app.route("/thongtindatve", methods=['GET', 'POST'])
+def thongtindatve():
+    return render_template('thongtindatve.html',
+                           fullName=session['fullName'],
+                           email=session['email'],
+                           cccd=session['cccd'])
+
+
 
 @login.user_loader
 def load_user(user_id):
